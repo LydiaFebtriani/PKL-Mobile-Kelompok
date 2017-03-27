e616baa6e63b0d1ce0947604a9b115a17026dfe9
StephanieTania <7314072@student.unpar.ac.id>
2017-03-28 02:02:09 +0700

    Perbaikan tema sensor

--- app/src/main/java/com/pab/unpar/pklmobilekelompok/SensorData.java
+++ app/src/main/java/com/pab/unpar/pklmobilekelompok/SensorData.java
@@ -9,48 +9,50 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
-public class SensorData implements SensorEventListener {
+public class SensorData{
     private Activity activity;
     private SensorManager manager;
     private Sensor sensor;
     private SharedPreferences sp;
+    private SensorEventListener listener;
 
-    public SensorData(Activity activity, SensorManager manager){
+    public SensorData(final Activity activity, SensorManager manager){
         this.activity=activity;
         Log.d("TEST","constructor");
         this.manager=manager;
         sensor=manager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        manager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+        listener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float val=event.values[0];
+                if(sp.getBoolean("useSensor",false)){
+                    if(val<10){
+                        Utils.changeToTheme(activity, Utils.THEME_DEFAULT_BLACK);
+                        SharedPreferences.Editor ed=activity.getSharedPreferences("dataProduk", Context.MODE_PRIVATE).edit();
+                        ed.putString("themeCode", Utils.THEME_DEFAULT_BLACK + "");
+                        ed.commit();
+                    }
+                    else{
+                        Utils.changeToTheme(activity, Utils.THEME_DEFAULT_WHITE);
+                        SharedPreferences.Editor ed=activity.getSharedPreferences("dataProduk", Context.MODE_PRIVATE).edit();
+                        ed.putString("themeCode", Utils.THEME_DEFAULT_WHITE + "");
+                        ed.commit();
+                    }
+                }
+                Log.d("Cahaya",val+"");
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        manager.registerListener(listener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
         sp=activity.getSharedPreferences("dataProduk",Context.MODE_PRIVATE);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        float val=event.values[0];
-        if(sp.getBoolean("useSensor",false)){
-            if(val<10){
-                Utils.changeToTheme(activity, Utils.THEME_DEFAULT_BLACK);
-                SharedPreferences.Editor ed=activity.getSharedPreferences("dataProduk", Context.MODE_PRIVATE).edit();
-                ed.putString("themeCode", Utils.THEME_DEFAULT_BLACK + "");
-                ed.commit();
-            }
-            else{
-                Utils.changeToTheme(activity, Utils.THEME_DEFAULT_WHITE);
-                SharedPreferences.Editor ed=activity.getSharedPreferences("dataProduk", Context.MODE_PRIVATE).edit();
-                ed.putString("themeCode", Utils.THEME_DEFAULT_WHITE + "");
-                ed.commit();
-            }
-        }
-        Log.d("Cahaya",val+"");
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
     public void unregisterSensor() {
-        manager.unregisterListener(this);
+        manager.unregisterListener(listener);
         Log.d("Sensor", "Unregistered");
     }
 
